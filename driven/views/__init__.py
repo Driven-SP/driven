e67908ce8789878aca79ce8833779fd927456b0b
prabinspkt <32003540+prabinspkt@users.noreply.github.com>
2021-02-17 22:46:13 -0500

    initialize bootstrap in the project (#5)
    now, we can fill in the content block in specific pages, and they
    will have all other common components in them

--- driven/views/__init__.py
+++ driven/views/__init__.py
@@ -3,6 +3,7 @@ from driven.views import index
 from driven.views import users
 from driven.views import address
 from driven.views import vendors
+from driven.views import profile
 from driven.views import addressvendorsmap
 
 blueprint = Blueprint('views', __name__)
@@ -10,8 +11,8 @@ index.views(blueprint)
 users.views(blueprint)
 address.views(blueprint)
 vendors.views(blueprint)
+profile.views(blueprint)
 addressvendorsmap.views(blueprint)
-
 
 def init_app(app):
     app.register_blueprint(blueprint)
