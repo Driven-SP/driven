8a7ed011c46fd2061f38359725e4194e606ac2a8
prabinspkt <prabin4998@gmail.com>
2021-02-18 14:15:33 -0500

    add a basic login page
    this is an example of how easy it is to add a component

--- driven/views/__init__.py
+++ driven/views/__init__.py
@@ -4,6 +4,7 @@ from driven.views import users
 from driven.views import address
 from driven.views import vendors
 from driven.views import profile
+from driven.views import login
 from driven.views import addressvendorsmap
 
 blueprint = Blueprint('views', __name__)
@@ -12,6 +13,7 @@ users.views(blueprint)
 address.views(blueprint)
 vendors.views(blueprint)
 profile.views(blueprint)
+login.views(blueprint)
 addressvendorsmap.views(blueprint)
 
 def init_app(app):
