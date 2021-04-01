155e17d7c0f621d93226858494d5aabf464a0032
prabinspkt <prabin4998@gmail.com>
2021-03-31 20:24:00 -0400

    use address APIs in frontend

--- driven/views/address.py
+++ driven/views/address.py
@@ -1,8 +1,8 @@
 from flask import render_template, request, session, redirect
-from  driven.firestore_api import getIdAddressMap, getPrimaryAddress, addAddressUser
+from  driven.firestore_api import getIdAddressMap, getPrimaryAddress, addAddressUser, deleteAddressUser, reviveAddressUser, changePrimaryAddressUser
 
 def views(bp):
-    @bp.route("/address", methods= ['GET'])
+    @bp.route("/address", methods= ['GET', 'POST'])
     def viewAddress():
         try:
             curr_username = session["username"]
@@ -11,21 +11,44 @@ def views(bp):
             user_active_id_and_addresses = getIdAddressMap(curr_user_doc_id, "ACTIVE")
             user_inactive_id_and_addresses = getIdAddressMap(curr_user_doc_id, "INACTIVE")
 
-            return render_template("profile.html", name=curr_username, primary_address=user_primary_address, active_id_and_addresses=user_active_id_and_addresses, inactive_id_and_addresses=user_inactive_id_and_addresses)
+            return render_template("address.html", name=curr_username, primary_address=user_primary_address, active_id_and_addresses=user_active_id_and_addresses, inactive_id_and_addresses=user_inactive_id_and_addresses)
 
         except:
             return redirect("/login")
  
-    @bp.route("/address/remove")
+    @bp.route("/removeAddress", methods=['POST'])
     def removeAddress():
-        pass
+        try:
+            user_document_id = session["user_document_id"]
+            address_id = request.form.get("address_id")
+            deleteAddressUser(user_document_id, address_id)
+            return redirect("/address")
+        except:
+            #  todo: maybe redirect to login page cause it is possible that the user is not logged in
+            return redirect("/address")
 
-    @bp.route("/address/revive")
+    @bp.route("/reviveAddress", methods=['POST'])
     def reviveAddress():
-        pass
+        try:
+            user_document_id = session["user_document_id"]
+            address_id = request.form.get("revive_address_id")
+            reviveAddressUser(user_document_id, address_id)
+            return redirect("/address")
+        except:
+            #  todo: maybe redirect to login page cause it is possible that the user is not logged in
+            return redirect("/address")
 
-    @bp.route("/address/changePrimary")
+    @bp.route("/changePrimaryAddress", methods=['POST'])
     def changePrimaryAddress():
+        try:
+            user_document_id = session["user_document_id"]
+            address_id = request.form.get("address_id")
+            changePrimaryAddressUser(user_document_id, address_id)
+            return redirect("/address")
+        except:
+            #  todo: maybe redirect to login page cause it is possible that the user is not logged in
+            return redirect("/address")
+
         pass
 
     @bp.route("/add_address", methods= ['POST', 'GET'])
@@ -56,3 +79,4 @@ def views(bp):
 
             #  if successful insertion, show the user's current address
             return redirect("/profile")
+
